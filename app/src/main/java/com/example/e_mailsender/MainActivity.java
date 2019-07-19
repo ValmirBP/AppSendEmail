8cf167d20138260044b91867347890025a66a4da
Valmir <valmir.barros57@yahoo.com.br>
2019-07-19 13:26:10 -0300

    UI/UX improvement

--- app/src/main/java/com/example/e_mailsender/MainActivity.java
+++ app/src/main/java/com/example/e_mailsender/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.e_mailsender;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText edtTo, edtSub, edtMsg;
+    TextView txtTo,txtSub,txtMsg;
     Button btnSend;
     String emailDest, assunto, msg;
     Intent intent;
@@ -18,13 +21,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtTo = findViewById(R.id.txtTo);
+        txtMsg = findViewById(R.id.txtMsg);
+        txtSub = findViewById(R.id.txtSub);
+        btnSend = findViewById(R.id.btnSend);
 
+// Inserção de um novo tipo de fonte no layout.
+
+        Typeface font = Typeface.createFromAsset(getAssets(),
+                "print_bold_tt.ttf");
+
+        txtTo.setTypeface(font);
+        txtMsg.setTypeface(font);
+        txtSub.setTypeface(font);
+        btnSend.setTypeface(font);
 
         edtTo = findViewById(R.id.edtTo);
         edtSub = findViewById(R.id.edtSub);
         edtMsg = findViewById(R.id.edtMsg);
 
-        btnSend = findViewById(R.id.btnSend);
+
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
